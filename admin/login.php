1026ca5338ee34f09161e93772eb8406a34af673
YH ONG <ongyh97@gmail.com>
2020-07-06 17:19:06 +0800

    initial commit

--- admin/login.php
+++ admin/login.php
@@ -18,12 +18,12 @@ include('./includes/path.inc.php');
                 <form name="login_form" method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
                     <div class="form-group">
                         <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                        <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" value="admin@admin.com">
                         <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                     </div>
                     <div class="form-group">
                         <label for="exampleInputPassword1">Password</label>
-                        <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                        <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password" value="Admin@123">
                     </div>
                     <button type="submit" name="loginbtn" class="btn btn-primary btn-block button">Log In</button>
                 </form>
